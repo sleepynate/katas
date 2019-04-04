53adfae1abd6e304f93259e644f784400f2e1d87
Nathan Dotz <nathan.dotz@gmail.com>
2019-04-04 15:01:50 -0400

    py/Bowling - add handling for '-' missed rolls

--- python/bowling/bowling.py
+++ python/bowling/bowling.py
@@ -24,6 +24,14 @@ class BowlingScoreTests(unittest.TestCase):
         """Can score a game with only one roll"""
         self.assertEqual(Game('1').score(), 1)
 
+    def test_can_score_almost_as_short_game(self):
+        """Can score a game with only two rolls"""
+        self.assertEqual(Game('12').score(), 3)
+
+    def test_can_score_a_good_frame_then_quit(self):
+        """Can score a game with only one roll"""
+        self.assertEqual(Game('9-').score(), 9)
+
 class FlattenTests(unittest.TestCase):
     def test_can_flatten_list(self):
         """Can flatten a list of lists to a 1-dimensional list"""
@@ -39,4 +47,7 @@ class Game:
         self.rolls = flatten(scoresheet_by_frame)
 
     def score(self):
-        return sum(map(int, self.rolls))
+        sum = 0
+        for roll in self.rolls:
+            sum += 0 if roll == '-' else int(roll)
+        return sum
