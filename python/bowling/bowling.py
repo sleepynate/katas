fec4bb105a68dee70ef8d5584b401e13200f759f
Nathan Dotz <nathan.dotz@gmail.com>
2019-04-04 14:43:52 -0400

    py/Bowling - add parsing of sheet with no pickups / split frames

--- python/bowling/bowling.py
+++ python/bowling/bowling.py
@@ -6,10 +6,25 @@ import unittest
 class BowlingScoreTests(unittest.TestCase):
 
     def test_can_parse_all_strike_score_sheet(self):
+        """Can parse score sheet with all strikes"""
         roll_count = len(Game('X X X X X X X X X X X X').rolls)
         self.assertEqual(roll_count, 12)
+
+    def test_can_parse_no_pickup_score_sheet(self):
+        """Can parse score sheet with no pickup rolls"""
+        roll_count = len(Game('9- 9- 9- 9- 9- 9- 9- 9- 9- 9-').rolls)
+        self.assertEqual(roll_count, 20)
+
+class FlattenTests(unittest.TestCase):
+    def test_can_flatten_list(self):
+        """Can flatten a list of lists to a 1-dimensional list"""
+        self.assertEqual(flatten([[1,2], [3,4]]), [1,2,3,4])
+
+def flatten(lst):
+    return [item for sublist in lst for item in sublist]
 
 class Game:
 
     def __init__(self, scoresheet):
-        self.rolls = scoresheet.split(' ')
+        scoresheet_by_frame = map(lambda x: list(x), scoresheet.split(' '))
+        self.rolls = flatten(scoresheet_by_frame)
